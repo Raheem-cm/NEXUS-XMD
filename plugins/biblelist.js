a025f66fe511d6c019c71be6a38bcea7045fce0c
Raheem-cm <nyoniraheem@gmail.com>
2025-06-03 00:37:12 +0300

    Update biblelist.js

--- plugins/biblelist.js
+++ plugins/biblelist.js
@@ -85,7 +85,7 @@ cmd({
 27. Revelation
 
 
-🇰🇪BY  NEXUS XMD🇰🇪
+🇹🇿BY  RAHEEM CMD🇹🇿
 `;
 
         // Remplacer ce lien par l'URL de l'image que tu m'enverras
@@ -99,7 +99,7 @@ cmd({
         // Envoi de la réponse avec l'image et la liste des livres de la Bible
         await conn.sendMessage(m.chat, {
             image: { url: imageUrl },
-            caption: `📖 *BIBLE LIST BY NEXUS XMD*:\n\n` +
+            caption: `📖 *BIBLE LIST BY RAHEEM CMD*:\n\n` +
                      `Here is the complete list of books in the Bible:\n\n` +
                      bibleList.trim() // Ajout du texte des livres de la Bible
         }, { quoted: mek });
