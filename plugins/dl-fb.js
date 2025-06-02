3348aaa4ce24236daabe48db1ab6d18b161c4c16
Raheem-cm <nyoniraheem@gmail.com>
2025-06-03 00:49:33 +0300

    Update dl-fb.js

--- plugins/dl-fb.js
+++ plugins/dl-fb.js
@@ -31,7 +31,7 @@ cmd({
     const videoUrl = data.data.url;
     await conn.sendMessage(from, {
       video: { url: videoUrl },
-      caption: "📥 *Facebook Video Downloaded*\n\n- Powered By Nexus Tech ✅",
+      caption: "📥 *Facebook Video Downloaded*\n\n- Powered By Raheem Cm ✅",
     }, { quoted: m });
 
   } catch (error) {
