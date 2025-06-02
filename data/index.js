66495ebafb7c7098b6cc1f1f8418b549065b1f84
Raheem-cm <nyoniraheem@gmail.com>
2025-06-02 11:41:46 +0300

    Update index.js

--- data/index.js
+++ data/index.js
@@ -1,8 +1,19 @@
-const { AntiDelDB,
+/**
+ * RAHEEM-CMD WhatsApp Bot – Data Utilities
+ * Author: +255763111390
+ * GitHub: https://github.com/Raheem-cm/RAHEEM-CMD
+ * Description:
+ * This module centralizes data storage, anti-delete settings, and group/user message tracking.
+ */
+
+const {
+    AntiDelDB,
     initializeAntiDeleteSettings,
     setAnti,
     getAnti,
-    getAllAntiDeleteSettings, } = require('./antidel');
+    getAllAntiDeleteSettings,
+} = require('./antidel');
+
 const {
     saveContact,
     loadMessage,
@@ -16,13 +27,15 @@ const {
     saveMessage,
 } = require('./store');
 
-
 module.exports = {
+    // Anti-delete features
     AntiDelDB,
     initializeAntiDeleteSettings,
     setAnti,
     getAnti,
     getAllAntiDeleteSettings,
+
+    // Chat storage & tracking
     saveContact,
     loadMessage,
     getName,
